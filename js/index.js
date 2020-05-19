be9700a34bfac121fe57ecf9b0a72a7c174dba3d
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-05-18 21:11:14 -0400

    adding slick slider

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$('.quotes__list').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.quotes__nav'
+  });
+  $('.quotes__nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.quotes__list',
+    focusOnSelect: true,
+    arrows: true,
+    centerMode: true,
+    prevArrow: "<img src='../images/prev-arrow.svg' class='prevButton'/>",
+    nextArrow: "<img src='../images/next-arrow.svg' class='nextButton'/>",
+  });
+      
